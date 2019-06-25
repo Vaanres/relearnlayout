a4a0c0ad21c04419b17ea9ca3db22c9e8beb0890
Vaanres <Vaanres@Vaanres-Mac.local>
2019-06-25 09:20:16 +0700

    update

--- pages/stack.vue
+++ pages/stack.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Stack</h1>
+  <div class="container">
+    <h1>Stack</h1>
+
+    <div class="stack">
+      <div><!-- child --></div>
+      <div><!-- child --></div>
+      <div class="stack">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+      <div><!-- etc --></div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+[class^='stack'] > * {
+  margin: 0;
+}
+.stack {
+  --space: 1.5rem;
+
+  & * {
+    background: transparentize(#000000, 0.9);
+    min-height: 50px;
+  }
+
+  & * + * {
+    margin-top: var(--space);
+  }
+}
+</style>
