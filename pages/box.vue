260fe8ee41a9a1369b3247d9ba5f0483d48ac500
Vaanres <Vaanres@Vaanres-Mac.local>
2019-10-10 17:34:42 +0700

    update

--- pages/box.vue
+++ pages/box.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Box</h1>
+  <div>
+    <h1>Box</h1>
+    <img src="/fm1.svg" alt="ha" />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,54 @@
   <div class="container">
     <ul class="mt-5">
       <li>
-        <h2><nuxt-link to="/cssisamazing">CSS is Amazing</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/breakingout">Breaking Out</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/stack">Stack</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/cssisamazing">CSS is Amazing</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Box">Box</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/stack">Stack</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Center">Center</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/Box">Box</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Cluster">Cluster</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/Center">Center</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Sidebar">Sidebar</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/Cluster">Cluster</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/SwitchBar">SwitchBar</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/Sidebar">Sidebar</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Cover">Cover</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/SwitchBar">SwitchBar</nuxt-link>
+        </h2>
       </li>
       <li>
-        <h2><nuxt-link to="/Grid">Grid</nuxt-link></h2>
+        <h2>
+          <nuxt-link to="/Cover">Cover</nuxt-link>
+        </h2>
+      </li>
+      <li>
+        <h2>
+          <nuxt-link to="/Grid">Grid</nuxt-link>
+        </h2>
       </li>
     </ul>
   </div>
